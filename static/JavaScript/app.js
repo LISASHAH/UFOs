577fc0d6405168e39de8b5262953d72b7e6ee464
LISASHAH <57725386+LISASHAH@users.noreply.github.com>
2020-02-09 09:53:30 -0800

    Add files via upload

--- static/JavaScript/app.js
+++ static/JavaScript/app.js
@@ -1,1 +1,74 @@
+// from data.js get table references
+const tableData = data
+var tbody = d3.select("tbody");
+
+ function buildTable(data) {
+// First, clear out any existing data
+   tbody.html("");
+   // Next, loop through each object in the data and append a row and cells for each value in the row
+   data.forEach((dataRow) => {
+     // Append a row to the table body
+     let row = tbody.append("tr");
+     // Loop through each field in the dataRow and add each value as a table cell (td)
+     Object.values(dataRow).forEach((val) => {
+       let cell = row.append("td");
+       cell.text(val);
+     });
+   });
+ }
+
+/// Keep track of all filters
+var filters = {};
+/// This function will replace your handleClick function
+function updateFilters() {
+  /// Save the element, value, and id of the filter that was changed
+  
+  let elementChanged = d3.select(this);
+  let elementValue = elementChanged.property("value");
+  let filterID = elementChanged.attr("id");
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if(elementValue)
+  {
+    filters[filterID]=elementValue;
+  }
+  else
+  {
+    delete filters[filterID];
+  };
+
+/// Call function to apply all filters and rebuild the table
+  filterTable(filters);
+}
+function filterTable(filters) {
+  let filteredData = tableData;
+  
+  Object.entries(filters).forEach(([key, value]) =>
+  {
+    console.log(key, value);
+    /// matches the filter values
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+ 
+  /// Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
+}
+/// Attach an event to listen for changes to each filter
+/// Hint: You'll need to select the event and what it is listening for within each set of parenthesis
+//d3.selectAll().on();
+/// Build the table when the page loads
+
+d3.selectAll("input").on("change", updateFilters);
+buildTable(tableData);
+
+
+
+
+
+
+
+
+
+
+
 
